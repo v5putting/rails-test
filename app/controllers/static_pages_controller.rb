616fb45d054f9e397ed999b3154f9f2d1a85ec4f
v5putting <957414954@qq.com>
2018-11-03 10:32:02 +0800

    adding homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def news
   end
+
+  def home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/about', to: "users#index"
+  get 'static_pages/about'
   get 'static_pages/contract'
   get 'static_pages/news'
   get 'users/index'
+  get 'static_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
